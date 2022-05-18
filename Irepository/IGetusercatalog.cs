033c681549a0f76674ea48bc65f11afd5b14481b
Bibin Scaria <bscaria@suyati.com>
2022-05-18 22:12:53 +0530

    userdata

---
+++ Irepository/IGetusercatalog.cs
@@ -0,0 +1,15 @@
+﻿using DomainLayer;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Irepository
+{
+   public interface IProduct: IGenericRepositoryOperation<Registration>
+    {
+
+    }
+}
